3c516f4f87521f6ea9faf27212b91bb63310f1a6
kimsia <kimcity@gmail.com>
2014-04-23 23:25:41 +0800

    load the test data file X_test.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,10 @@
 ## Merges the training and the test sets to create one data set
 merge_sets <- function(directory) {
+  path <- paste("./", directory, "/test/X_test.txt", sep="")
+  test_data <- read.table(path)
+  return (test_data)
 }
+test_data <- merge_sets("UCI HAR Dataset")
 ## Extracts only the measurements on the mean and standard deviation for each measurement
 ## Uses descriptive activity names to name the activities in the data set
 ## Appropriately labels the data set with descriptive activity names
