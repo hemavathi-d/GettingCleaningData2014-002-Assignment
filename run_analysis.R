ca1fb82b2e16dc5cdb03a0d751adc1a39ad49f98
kimsia <kimcity@gmail.com>
2014-04-23 22:15:25 +0800

    include the original raw data and added comments to the run_analysis.R and the README

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+## Merges the training and the test sets to create one data set
+merge_sets <- function(directory) {
+}
+## Extracts only the measurements on the mean and standard deviation for each measurement
+## Uses descriptive activity names to name the activities in the data set
+## Appropriately labels the data set with descriptive activity names
+## Creates a second, independent tidy data set with the average of each variable for each activity and each subject
