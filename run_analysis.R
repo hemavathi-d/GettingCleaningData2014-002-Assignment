46a248bd4bf60e74cf9e6c9104afeae410a2b162
kimsia <kimcity@gmail.com>
2014-04-24 22:57:31 +0800

    added function to merge the training and test sets to create one data set

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,12 @@
 merge_sets <- function(directory) {
   path <- paste("./", directory, "/test/X_test.txt", sep="")
   test_data <- read.table(path)
-  return (test_data)
+  path <- paste("./", directory, "/train/X_train.txt", sep="")
+  train_data <- read.table(path)
+  merged_data <- merge(train_data, test_data, all.x=TRUE)
+  return (merged_data)
 }
-test_data <- merge_sets("UCI HAR Dataset")
+merged_data <- merge_sets("UCI HAR Dataset")
 ## Extracts only the measurements on the mean and standard deviation for each measurement
 ## Uses descriptive activity names to name the activities in the data set
 ## Appropriately labels the data set with descriptive activity names
